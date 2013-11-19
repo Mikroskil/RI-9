581b180465849ee7bd65fe92678921d31efba265
Radumta Sitepu <rai_zet@hotmail.com>
2013-11-19 20:49:46 +0700

    commit8

---
+++ fungsi.php
@@ -0,0 +1,66 @@
+<?php
+  session_start();
+  function config()
+  {
+    $host = 'localhost';
+    $user = 'root';
+    $password = '';
+    $db = 'seputar';
+    $conn = @mysql_connect($host,$user,$password) or die('Koneksi Database Gagal!');
+    mysql_select_db($db,$conn);
+	return $conn;
+  }
+
+
+
+function kepala()
+{
+echo'
+<html>
+<head><title>e-Booking System Mata Kuliah Pengganti</title>
+	<link rel="stylesheet" href="css/layout.css" type="text/css">
+    <script src="js/jquery.js"></script>
+    <script src="js/menu.js"></script>
+
+</head>
+<body>
+	<div id="container">';
+}
+
+
+function navigasi()
+{
+	echo'
+	    <div id="clear"></div>
+		<div><script>menu_bar();</script></div>
+		<div id="clear"></div>';
+}
+
+function gadgetkiri()
+{
+	echo'<div id="gadget_kiritampil"></div>';
+}
+
+function gadgetkanan()
+{
+	echo'<div id="gadget_kanan"></div>';
+}
+
+function footer()
+{
+	echo'
+		<div id="clear"></div>
+		<div id="wrapper-footer">
+
+					<div id="footer-bottom">
+						<p class="left">www.e-bookingsytemmatkulpengganti.com</p>
+						<p class="right">Design By : &copy Radumta Sitepu</p>
+					</div>
+		</div>
+
+	</div>
+</body>
+</html>';
+}
+
+?>
--- index.php
+++ index.php
@@ -1,21 +1,8 @@
-<html>
-<head>
-	<title>E-booking System Mata Kuliah Pengganti</title>
-
-	<link rel="stylesheet" href="css/layout.css" type="text/css">
-</head>
-<body>
-<div id="container">
-	<div id='headerlog'><h1>Sign In</h1></div>
-	<div id='loginbox'>
-	<form>
-		username</br>
-		<input type='text' name='username' ></br>
-		password:</br>
-		<input type='password' name='password'></br>
-		<input type='button' name='signin' value='Sign In'>
-	</form>
-	</div>
-</div>
-</body>
-</html>
+<?php
+require_once"fungsi.php";
+kepala();
+navigasi();
+gadgetkiri();
+gadgetkanan();
+footer();
+?>

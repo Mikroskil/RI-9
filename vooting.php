8b93f4cdd9b02118ab147b40e6f4d0a06a149e9e
Radumta Sitepu <rai_zet@hotmail.com>
2013-12-04 21:01:07 +0700

    Tambah Fitur Vooting

---
+++ vooting.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+echo'
+  <div id="vooting">
+	<h4 align="center">Apakah Sistem Ini Berguna</h4>
+	<form action="" method="post">
+	  <table bordercolor="#111111" width="290" align="left" border="0">
+		<tr>
+			<td><input type="radio" name="voting" value="Tidak">Tidak</td>
+		</tr>
+		<tr>
+			<td><input type="radio" name="voting" value="Ya">Ya</td>
+		</tr>
+		<tr>
+			<td><input type="radio" name="voting" value="Sedang">Ragu-Ragu</td>
+		</tr>
+		<tr>
+          <td><input type="submit" name="submit" value="Vote"></td>
+        </tr>
+
+       </table>
+   </form>
+ </div>';
+
+ ?>

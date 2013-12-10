d334d6a945be76e75122779182fd99a042262d41
Radumta Sitepu <rai_zet@hotmail.com>
2013-12-10 21:28:04 +0700

    Edit Fungsi document.write menjadi write

--- js/menu.js
+++ js/menu.js
@@ -9,23 +9,24 @@ function hide(button, menu)
   document.getElementById(menu).style.visibility= "hidden";
 
 }
+function write(a)
+{
+	document.write(a);
+}
 
 function menu_bar()
 {
-	document.write('  <div class="menu">');
-	document.write('  <form>');
-	document.write('  <table border="0" cellspacing="1" width="1024">');
-	document.write('    <tr>');
+	write('<div class="menu">');
+	write('<form>');
+	write('  <table border="0" cellspacing="1" width="1024">');
+	write('    <tr>');
 	
-	document.write('    <td class="menu" id="home" onmouseover="select(\'home\')" onmouseout="disselect(\'home\')" onclick="click(\'home\')"><a href="index.php">BOOKING JADWAL</a></td>');
-	
-	document.write('    <td class="menu" id="home" onmouseover="select(\'home\')" onmouseout="disselect(\'home\')" onclick="click(\'home\')"><a href="index.php">JADWAL</a></td>');
-	
-	
-	document.write('  </tr>');
-	document.write('  </table>');
-	document.write('</form>');
-	document.write('</div>');
+	write('    <td class="menu" id="home" onmouseover="select(\'home\')" onmouseout="disselect(\'home\')" onclick="click(\'home\')"><a href="index.php">BOOKING JADWAL</a></td>');
+	write('    <td class="menu" id="home" onmouseover="select(\'home\')" onmouseout="disselect(\'home\')" onclick="click(\'home\')"><a href="index.php">JADWAL</a></td>');
+	write('  </tr>');
+	write('  </table>');
+	write('</form>');
+	write('</div>');
 	
 
 
